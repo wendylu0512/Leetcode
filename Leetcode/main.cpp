0c6e507e10c6c12320589bffd811faf26c6b5099
Wendi Lu <wendilu@Wendis-MacBook-Pro.local>
2014-10-22 13:32:33 -0400

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by Wendi Lu on 10/22/14.
+//  Copyright (c) 2014 Wendi Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
