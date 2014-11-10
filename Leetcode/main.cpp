c63f78b2ab57778f4a1d5677d1476f14a7fa4750
wendylu0512 <wendylu0512@gmail.com>
2014-11-10 00:36:54 -0500

    test commit

--- Leetcode/main.cpp
+++ Leetcode/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, const char * argv[])
 {
     string s = "+---...2x+--x--+x-+-x2...---+";
     reverseWords(s);
-    cout << s << endl;
+   // cout << s << endl;
     return 0;
 }
 
